86d93c3c69692ab439b5a8e41e998e1676964ca3
Libby Hemphill <libbyh@gmail.com>
2014-03-20 11:05:24 -0500

    improved parse function
    error handling, file cleanup

--- getTimesArticles.py
+++ getTimesArticles.py
@@ -68,39 +68,56 @@ def getArticles(date, api_key, json_file_path):
 # parse the JSON files you stored into a tab-delimited file
 def parseArticles(date, csv_file_name, json_file_path):
     for file_number in range(101):
-        # get the articles just fetched and put them into a dictionary
-        file_name = getJsonFileName(date,file_number, json_file_path)
-        in_file = open(file_name, 'r')
-        articles = convert(json.loads(in_file.read()))
-        in_file.close()
+        # get the articles and put them into a dictionary
+        try:
+            file_name = getJsonFileName(date,file_number, json_file_path)
+            in_file = open(file_name, 'r')
+            articles = convert(json.loads(in_file.read()))
+            in_file.close()
+        except IOError as e:
+			logging.error("IOError in %s page %s: %s %s", date, file_number, e.errno, e.strerror)
+			continue
+            
+        # open the CSV for appending
+        try:
+            out_file = open(csv_file_name, 'ab')
+        except IOError as e:
+			logging.error("IOError: %s %s", date, file_number, e.errno, e.strerror)
+			continue
         
-        # loop through the articles putting what we need in a CSV
-        out_file = open(csv_file_name, 'ab')
+        # loop through the articles putting what we need in a CSV   
         try:
             for article in articles["response"]["docs"]:
-                keywords = ""
                 # if (article["source"] == "The New York Times" and article["document_type"] == "article"):
-                if "keywords" in articles.keys():   
-                    keywords = getMultiples(article["keywords"],"value")
+                keywords = ""
+                keywords = getMultiples(article["keywords"],"value")
     
                 # should probably pull these if/else checks into a module
                 variables = [
-                    article["pub_date"] if article["pub_date"] in article.keys() else "", 
+                    article["pub_date"], 
                     keywords, 
-                    str(article["headline"]["main"]).decode("utf8") if "main" in article["headline"].keys() else "", 
+                    str(article["headline"]["main"]).decode("utf8").replace("\n","") if "main" in article["headline"].keys() else "", 
                     str(article["source"]).decode("utf8") if "source" in article.keys() else "", 
                     str(article["document_type"]).decode("utf8") if "document_type" in article.keys() else "", 
                     article["web_url"] if "web_url" in article.keys() else "",
                     str(article["news_desk"]).decode("utf8") if "news_desk" in article.keys() else "",
                     str(article["section_name"]).decode("utf8") if "section_name" in article.keys() else "",
-                    str(article["snippet"]).decode("utf8") if "snippet" in article.keys() else "",
-                    str(article["lead_paragraph"]).decode("utf8") if "lead_paragraph" in article.keys() else "",
+                    str(article["snippet"]).decode("utf8").replace("\n","") if "snippet" in article.keys() else "",
+                    str(article["lead_paragraph"]).decode("utf8").replace("\n","") if "lead_paragraph" in article.keys() else "",
                     ]
                 line = "\t".join(variables)
                 out_file.write(line.encode("utf8")+"\n")
-        except KeyError:
-            logging.error("KeyError in %s page %s", date, file_number)
-
+        except KeyError as e:
+            logging.error("KeyError in %s page %s: %s %s", date, file_number, e.errno, e.strerror)
+            continue
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except: 
+            logging.error("Error on %s page %s: %s", date, file_number, sys.exc_info()[0])
+            continue
+        
+        out_file.close()
+        
 # Main function where stuff gets done
 
 def main():
